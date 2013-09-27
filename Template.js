7a668e2779f73f03da875f871cbf1492df2d8966
Victor Hugo <victorvhpg@gmail.com>
2013-09-26 23:24:26 -0300

    :bomb: alterar algoritmo de substituicao de valores

--- Template.js
+++ Template.js
@@ -12,7 +12,7 @@ var Template = (function(window) {
         parametroInicio: "{@",
         parametroFim: "}"
     },
-    document = window.document, Template, $ = window.jQuery;
+    document = window.document, Template, $ = window.jQuery, JSON = window.JSON;
 
     Template = function(config) {
         this.config = $.extend({}, _configPadrao, config);
@@ -30,29 +30,44 @@ var Template = (function(window) {
                     parametroInicio = this.config.parametroInicio,
                     parametroFim = this.config.parametroFim,
                     serializaObj = this.config.serializaObj,
-                    template = parametros.template, item;
+                    template = parametros.template, item, $linhaHTML;
             if (!$.isArray(vetObjDados)) {
                 vetObjDados = [vetObjDados];
             }
             for (var i = 0, len = vetObjDados.length; i < len; i++) {
-                item = template;
-                for (var j in vetObjDados[i]) {
-                    item = item.replace(new RegExp(parametroInicio + j + parametroFim, "g"), vetObjDados[i][j]);
-                }
-                var $linhaHTML = $($.trim(item));
+                item = template.replace(new RegExp(parametroInicio + "(.*?)" + parametroFim, "g"), function(parametroCompleto, parametro) {
+                    var obj = vetObjDados[i],
+                            objs = parametro.split("."), args;
+                    parametro = objs[objs.length - 1];
+                    for (var j = 0, l = objs.length - 1; j < l; j++) {
+                        obj = obj[objs[j]];
+                    }
+                    parametro = parametro.replace(/\((.*?)\)/, function(parenteses, argumentos) {
+                        args = argumentos;
+                        return "";
+                    });
+                    return  (typeof obj[parametro] === "function") ? obj[parametro](args) : obj[parametro];
+                });
+                $linhaHTML = $($.trim(item));
                 serializaObj && $linhaHTML.attr("data-template-obj", JSON.stringify(vetObjDados[i]));
                 if (typeof aCadaLinha === "function") {
-                    $linhaHTML = aCadaLinha($linhaHTML, vetObjDados[i]);
+                    $linhaHTML = aCadaLinha($linhaHTML, vetObjDados[i]) || $linhaHTML;
                 }
                 $resultados.append($linhaHTML);
             }
             return $resultados;
         }
     };
-    
+
     Template.renderizar = function(parametros) {
         var tmpl = Template.getInstancia(parametros.config);
         return tmpl.renderizar(parametros);
-    };
+    };    
+    
+    if (typeof define === "function" && define.amd) {
+        define(function() {
+            return Template;
+        });
+    }
     return Template;
 })(window);
